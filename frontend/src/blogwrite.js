8a1ce74e1781aad0cfb6971df536dc9abdd86db4
leapfrog <jghimire.034@gmail.com>
2024-12-19 20:04:17 +0545

    fix bug

--- frontend/src/blogwrite.js
+++ frontend/src/blogwrite.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-function Blogwrite() {
+function Blogwritee() {
     var time=getdate();
      const location = useLocation();
     const navigate = useNavigate();     
